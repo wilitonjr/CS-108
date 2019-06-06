33074e59b17721b382276fb2c94cde7ef15bf0b4
Will Rodrigues <51224565+wilitonjr@users.noreply.github.com>
2019-06-06 14:48:08 -0700

    Add files via upload

--- program 2/Auto.java
+++ program 2/Auto.java
@@ -1,76 +1,94 @@
+/**
+ *  Program 2a
+ *  20.3 Program 2a: Auto Class
+ *  CS108
+ *  Jun 5 2019
+ *  @author  Will Rodrigues
+  */
+
 public class Auto {
 
+	//variables
 	String make;
 	String model;
 	int milesDriven;
 	double gallonsOfGas;
 	
+	//no-arg method
 	public Auto() {
 		
 	}
+	
+	//sets the values of variables
 	public Auto(String make, int startMiles, double startGallons) {
 		this.make = make;
 		this.model = "";
 		milesDriven = startMiles;
 		gallonsOfGas = startGallons;
 	}
+	
+	//sets the values of variables
 	public Auto(String make, String model, int startMiles, double startGallons) {
 		this.make = make;
 		this.model = model;
 		milesDriven = startMiles;
 		gallonsOfGas = startGallons;
 	}
+	
+	//returns make
 	public String getMake() {
 		return make;
 	}
+	
+	//returns method
 	public String getModel() {
 	   return model;
 	}
+	
+	//returns miles driven
 	public int getMilesDriven() {
 		return milesDriven;
 	}
+	
+	//returns gallon of gas
 	public double getGallonsOfGas() {
 		return gallonsOfGas;
 	}
+	
+	//sets miles driven
 	public void setMilesDriven(int milesDriven) {
 		this.milesDriven = milesDriven;
 	}
+	
+	//sets gallons of gas
 	public void setGallonsOfGas(double gallonsOfGas) {
 		this.gallonsOfGas = gallonsOfGas;
 	}
+	
+	//sets make
 	public void setMake(String make) {
 		this.make = make;
 	}
+	
+	//sets model
 	public void setModel(String model) {
 		this.model = model;
 	}
+	
+	//returns miles per gallon
 	public double calculateMilesPerGallon() {
 		return milesDriven / gallonsOfGas;
 	}
+	
+	//returns descriptive string
 	public String toString() {
-		return "" + make + " " + model + ", " + milesDriven + ", " + gallonsOfGas;
+		return "" + make + ", " + milesDriven + ", " + gallonsOfGas;
 	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Auto other = (Auto) obj;
-		if (Double.doubleToLongBits(gallonsOfGas) != Double.doubleToLongBits(other.gallonsOfGas))
-			return false;
-		if (milesDriven != other.milesDriven)
-			return false;
-		if (make == null) {
-			if (other.make != null)
-				return false;
-		} else if (!make.equals(other.make))
-			return false;
-		return true;
-	}
+	
+
 	public static void main(String[] args) {
+		
+		
 		Auto sportsCar = new Auto("Ferrari", "Fast", 0, 0.0 );
 		Auto sedan1 = new Auto("BMW", "X", 449, 13.9);
 		Auto sedan2 = new Auto("BMW", "i", 100, 15.0);
@@ -88,7 +106,7 @@ public class Auto {
 		}
 	}
 	
-	//(0) Returns a string containing the programming assignment and your name
+	// Returns a string containing the programming assignment and your name
 	public static String progId() {
 	      return "Program 2, Patty Kraft";
 	}
